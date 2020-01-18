699680d60e1bb2be37db2669178b932922588f68
Budreev Andrey <test-ru2007@ya.ru>
2020-01-18 19:18:33 +0300

    Commit2

--- get-blocksize/Script.ps1
+++ get-blocksize/Script.ps1
@@ -1,3 +1,27 @@
-﻿#
-# Script.ps1
-#
+﻿function get-blocksize 
+{   
+      [CmdletBinding()] 
+      Param
+      (
+      [Parameter(Mandatory=$True)] 
+      [string]$volume,
+      [int]$sampleinterval,
+      [int]$maxsamples 
+      )        
+        $outfile = 'c:\scripts\blocksize.csv' 
+        $array = @()
+        $perfomancesampledata = Get-Counter -Counter "\LogicalDisk(*)\Disk Bytes/sec", "\LogicalDisk(*)\Disk Transfers/sec" -SampleInterval $sampleinterval -MaxSamples $maxsamples        
+        foreach ($p in $perfomancesampledata )
+            {        
+                $throughput = ($p.CounterSamples |  ? instancename -match "$volume" | ? path -Match "bytes")  
+                $iops = ($p.CounterSamples |  ? instancename -match "$volume" | ? path -Match "transfers")        
+                $PerfomanceData = New-Object psobject
+                $PerfomanceData | Add-Member -type NoteProperty -name 'timestamp' -Value $p.TimeStamp
+                $PerfomanceData | Add-Member -type NoteProperty -name 'iops' -Value ("{0:n1}" -f ($iops.CookedValue))
+                $PerfomanceData | Add-Member -type NoteProperty -name 'throughput(KBps)' -Value ("{0:n1}" -f ($throughput.CookedValue / 1kb))
+                $PerfomanceData | Add-Member -type NoteProperty -name 'throughput(MBps)' -Value ("{0:n1}" -f ($throughput.CookedValue / 1mb))
+                $PerfomanceData | Add-Member -type NoteProperty -name 'blocksize(kb)' -Value ("{0:n1}" -f (($throughput.CookedValue / 1kb) / ($iops.CookedValue)))
+                $array+=$PerfomanceData                                       
+            }                    
+                $array | Export-Csv -Path $outfile -Delimiter "," -NoTypeInformation -Encoding UTF8 
+}
